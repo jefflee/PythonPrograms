2d4fd0b73f0bfeb98ef6f3892d523f64dbc94d39
JEFFZ16\Jeff <hellow.jeff@gmail.com>
2024-06-01 11:11:13 +0800

    More examples of input and output

--- Code/pythonPrograms/syntax/input.py
+++ Code/pythonPrograms/syntax/input.py
@@ -1,3 +1,30 @@
-# get input from console
+import time
+import sys
+
+print("======================  Print  =========================")
+print(1, 2, 3)           # 1 2 3
+print(1, 2, 3, sep=';')   # 1;2;3
+print(1, 2, 3, sep=';', end='!')
+print(1, 2, 3)           # 1;2;3!1 2 3
+
+print("======================  Loading  =========================")
+print('Loading', end='')
+for i in range(6):
+    print('.', end='', flush=True)
+    time.sleep(0.5)
+
+print("\n======================  Loading 2 | go to the first char position  =========================")
+n = 7
+for i in range(n+1):
+    print(f'\rCountdown {n-i:2} seconds', end='')
+    time.sleep(1)
+print('\rTime is up', end='')
+
+print("======================  Input from console  =========================")
 name = input('Hello, what is your name?  ')
 print('Hi,', name)
+
+
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+print(f'Sum={a+b}')
